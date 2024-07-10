f9fa9d32280b93afa5f34411d84d977978b87032
MosesMuiru <muirumoses9@gmail.com>
2024-07-10 13:55:45 +0300

    fixed the disabled button

--- lib/snl/game/bot.ex
+++ lib/snl/game/bot.ex
@@ -1,51 +1,26 @@
 defmodule Snl.Bot do
-
-
   use GenServer
   alias Snl.Game.Play
   alias Phoenix.PubSub
 
-  @moduledoc"""
-  
-    who is playing
-    1 -> the user
-    2 -> computer
-
-   i have to keep the state of who has played
-    state
-
-    pass the state of the user down the line,
-      how will you know the time for you to play
-      
-      when you pass 
-   
-    -----
-    here lets broadcast the state
-    that is the bot has played
-
-
-    
-  """
-
   def start_link(_) do
-    times_played = 0;
-    GenServer.start_link(__MODULE__, times_played, name: __MODULE__) 
+    times_played = 0
+    GenServer.start_link(__MODULE__, times_played, name: __MODULE__)
   end
 
   # i need to maintain the state
   def bot_roll_die(pid) do
-    GenServer.call(pid, :bot_roll_die)
+    GenServer.cast(pid, :bot_roll_die)
   end
 
-  #server
+  # server
   def init(times_played) do
     {:ok, times_played}
   end
 
-  def handle_call(:bot_roll_die, _from, times_played) do
-    roll_state = Play.roll_die() 
+  def handle_cast(:bot_roll_die, times_played) do
+    roll_state = Play.roll_die()
     PubSub.broadcast(Snl.PubSub, "playing", {:bot_roll, roll_state})
-    {:reply, roll_state, times_played+1}
+    {:noreply, times_played + 1}
   end
-
 end
--- lib/snl_web/live/snl_live.ex
+++ lib/snl_web/live/snl_live.ex
@@ -6,34 +6,47 @@ defmodule SnlWeb.SnlLive do
   alias Snl.Bot
 
   def mount(_params, _session, socket) do
-    IO.puts("this is the mount function")
+    Phoenix.PubSub.subscribe(Snl.PubSub, "playing")
 
-        Phoenix.PubSub.subscribe(Snl.PubSub, "playing")
-  	new_socket =
-  	socket
-  	|> assign(die: 0)
-  	|> assign(rand: Play.random_color())
+    new_socket =
+      socket
+      |> assign(die: 0)
+      |> assign(rand: Play.random_color())
+      |> assign(human_turn_to_play: true)
 
-  	{:ok, new_socket}
+    {:ok, new_socket}
   end
 
   def handle_event("roll", map, socket) do
-  	die_roll = Play.roll_die()
-        GenServer.call(Bot, :bot_roll_die)
+    die_roll = Play.roll_die()
+    Process.send_after(self(), :bot_roll, 3000)
 
-  	new_socket =
-  	socket
-  	|> assign(die: die_roll)
-        |> assign(human_played: true)
+    new_socket =
+      socket
+      |> assign(die: die_roll)
+      |> assign(human_turn_to_play: false)
 
-  	{:reply, map, new_socket}
+    {:reply, map, new_socket}
+  end
+
+  # The results of the bot roll should be returned after 3 seconds
+  def handle_info(:bot_roll, socket) do
+    GenServer.cast(Bot, :bot_roll_die)
+    {:noreply, socket}
   end
 
   def handle_info({:bot_roll, rolls}, socket) do
-    
-    IO.inspect(rolls)
-    IO.puts("this")
-    human_played = socket.assigns.human_played
-    {:noreply, socket}
+    new_socket =
+      case socket.assigns.human_turn_to_play do
+        false ->
+          socket
+          |> assign(human_turn_to_play: true)
+          |> assign(die: rolls)
+
+        true ->
+          socket
+      end
+
+    {:noreply, new_socket}
   end
 end
